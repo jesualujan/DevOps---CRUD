6a3f25fc15831bf3db16e77730a7452ac88dcdfc
jesualujan <lujan.oficial@gmail.com>
2024-09-25 15:27:20 -0600

    ✨ isertar datos

---
+++ ddl/bookTestData.sql
@@ -0,0 +1,8 @@
+insert into book (id, author, price, description, year_published)
+values ('f2c0581d-6d87-4872-a01d-074ac12a650d', 'Test author 1', 111, 'Dummy description', 1999);
+
+insert into book (id, author, price, description, year_published)
+values ('471bd3ef-9118-4cb8-ba05-ca41fa56c355', 'Test author 2', 222, 'Dummy description', 2022);
+
+insert into book (id, author, price, year_published)
+values ('7990ef32-915e-45fd-977d-3951e97c8b0b', 'Test author 3', 333, 2007);
