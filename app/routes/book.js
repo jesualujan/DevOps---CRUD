5553765e0e6ff028691b67119e0acb5cec1ab28c
jesualujan <lujan.oficial@gmail.com>
2024-09-25 15:31:14 -0600

    ✨ creación de las rutas

---
+++ app/routes/book.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+
+})
+
+router.get('/:bookId', (req, res) => {
+    
+})
+
+router.post('/:bookID', (req, res) => {
+    
+})
+
+router.patch('/', (req, res) => {
+    
+})
+
+router.delete('/', (req, res) => {
+    
+})
+
+module.exports = router
