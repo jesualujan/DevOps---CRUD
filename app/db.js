d49f496c0cef792686d94af5f187f1e2301c9886
jesualujan <lujan.oficial@gmail.com>
2024-09-25 14:23:37 -0600

    ✨ conexión a la base de datos

--- app/db.js
+++ app/db.js
@@ -1,13 +1,13 @@
 const { Client } = require('pg')
 
-const cliente = new Client ({
+const client = new Client ({
     user: process.env.POSTGRES_USER,
     password:  process.env.POSTGRES_PASSWORD,
     host:  process.env.POSTGRES_HOST
 })
 
-cliente.connect()
-cliente.query('select now()').then(
+client.connect()
+client.query('select now()').then(
     res => console.log('Conectado a la base de datos 💻' + res.row[0].now)
 )
 
